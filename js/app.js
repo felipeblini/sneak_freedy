73118ce0088ab5d9b3274e054eb9b6d9b32207ea
Jeffrey <jeff@btccorp.com.au>
2015-09-09 11:47:11 +1000

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+(function($){
+
+    $(window).load(function(){
+        $(".content").mCustomScrollbar({
+            scrollButtons: { enable: true },
+            theme: "light-3",
+        });
+    });
+
+})(jQuery);
